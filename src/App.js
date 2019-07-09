b305161ac1a27d5f168a3ad5dc2c0fce6af67f7e
Peter Tynan <pettynan@amazon.com>
2019-07-08 22:56:49 -0700

    frontend is functional on localhost. needs to be deployed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+
+import mockData from './mock.json';
+
+const API = 'http://localhost:5000/tasks';
+
+
+function Tasks() {
+
+  const [tasks, setTasks] = useState([]);
+  // const []
+
+
+  const _getTasks = () => {
+
+
+    fetch( API, {
+      mode: 'cors',
+    })
+    .then(data => data.json())
+    .then(tasks => setTasks(tasks))
+    .catch(console.error);
+
+  }
+
+  useEffect(_getTasks, []);
+
+  return(
+    <ul>
+      {tasks.map( task => 
+        <li key={task.id}>
+          <summary>
+            <div>Assigned to: {task.assignee}</div>
+            <div>Title: {task.title}</div>
+            <div>Description: {task.description}</div>
+            <div>Status: {task.status}</div>
+          </summary>
+        </li>
+        )}
+    </ul>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>TaskMaster</header>
+      <main>
+        <Tasks />
+      </main>
+      <footer>&copy; 2019 Petr</footer>
+    </>
   );
 }
 
